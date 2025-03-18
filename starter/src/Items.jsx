0c72f61537c49a0dae529a81112d665d85f04254
brherr <herrbrad@yahoo.com>
2025-03-18 12:27:38 -0400

    lesson 311 render data

--- starter/src/Items.jsx
+++ starter/src/Items.jsx
@@ -2,16 +2,23 @@ import SingleItem from "./SingleItem";
 import { useQuery } from "@tanstack/react-query";
 import customFetch from "./utils";
 
-const Items = ({ items }) => {
-  const result = useQuery({
+const Items = () => {
+  const { isLoading, data } = useQuery({
     queryKey: ["tasks"],
-    queryFn: () => customFetch.get("/"),
+    queryFn: async () => {
+      const { data } = await customFetch.get("/");
+      return data;
+    },
   });
-  console.log("🚀 ~ Items ~ result:", result);
+  console.log("🚀 ~ Items ~ data:", data);
+
+  if (isLoading) {
+    return <p style={{ marginTop: "1rem" }}>Loading...</p>;
+  }
 
   return (
     <div className="items">
-      {items.map((item) => {
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
